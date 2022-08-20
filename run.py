72c8e5c328b49ec60db4ab5d90f4b176940b335b
Tomasz <tomaszwoloszyn983@gmail.com>
2022-08-20 06:43:07 +0000

    Create about, contact and other subpages

--- run.py
+++ run.py
@@ -1,11 +1,34 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return "Hello World"
+    # ##app jest chyba jakąś Flask'ow adnotacją (route decorator) oznaczającą aplikację
+    # Funkcja zwraca plik index.html, któtrego Flask domyślnie szuka 
+    # w folderze templates.
+    return render_template("index.html")
+
+
+@app.route("/about")
+    # /about oznacza rozszerzenie gównego adresu strony (Home Page)
+    # pod tym rozszerzonym adresem będzie wyświetlana poniższa
+    # funkcja.
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def contact():
+    return render_template("careers.html")
+
 
 if __name__ == "__main__":
     app.run(
