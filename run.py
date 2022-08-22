5b0bbb0a43a185a4204f478ac7bba204e58584e1
Tomasz <tomaszwoloszyn983@gmail.com>
2022-08-22 07:47:34 +0000

    Add company.json file. Style About.html page

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # ##app jest chyba jakąś Flask'ow adnotacją (route decorator) oznaczającą aplikację
+    # @app jest chyba jakąś Flask'ow adnotacją (route decorator) oznaczającą aplikację
     # Funkcja zwraca plik index.html, któtrego Flask domyślnie szuka 
     # w folderze templates.
     return render_template("index.html")
@@ -16,18 +17,33 @@ def index():
     # /about oznacza rozszerzenie gównego adresu strony (Home Page)
     # pod tym rozszerzonym adresem będzie wyświetlana poniższa
     # funkcja.
+    # 
+    # Dalej do zmiennej data przypisujemy zawartość pobraną z pliku 
+    # company.json. "r" oznacza że otwieramy ten plik jako: tylko do 
+    # odczytu.
+    # render_template dodajemy nowy parametr company i przypisujemy 
+    # do niego zawartość data aby wyświetlić je na stronie.
+    # 
 def about():
-    return render_template("about.html")
+    data = []
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("about.html", page_title="About", company = data)
 
 
 @app.route("/contact")
+    # Dodaliśmy tutaj drugi parametr którym jest zmienna.
+    # Wartość tej zmiennej będzie wyświetlana jako header
+    # na stronie o powyższym adresie.
+    # W każdej stronie jako zawartość h2 podajemy nazwę tej zmiennej
+    # w podwójnym nawiasie klamrowym.
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
