73e3a15dcd450e7d29f550d0cf87ab5e5145673b
Tomasz <tomaszwoloszyn983@gmail.com>
2022-08-24 06:12:07 +0000

    Add gitignore and env.py files

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -31,13 +34,33 @@ def about():
     return render_template("about.html", page_title="About", company = data)
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
     # Dodaliśmy tutaj drugi parametr którym jest zmienna.
     # Wartość tej zmiennej będzie wyświetlana jako header
     # na stronie o powyższym adresie.
     # W każdej stronie jako zawartość h2 podajemy nazwę tej zmiennej
     # w podwójnym nawiasie klamrowym.
+    # 
+    # Wewnątrz funkcji definiujemy metodę POST, która będzie wysyłać dane wpisane
+    # w formularzu w dziale contact do serwera.
+    # Ważne jest aby zapamiętać o zaimportowaniu klasy request.
 def contact():
+    if request.method == "POST":
+        # print(request.form) to wyświetli wszystkie wpisane przez nas dane z formularza.
+        # print(request.form.get("name"))
+        # print(request.form.get("email"))
+        flash("Thanks {}, we have received your message!".format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
